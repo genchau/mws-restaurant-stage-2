498b1e016ff204b1df27370f98a532291a9e262c
genchau <35973459+genchau@users.noreply.github.com>
2018-06-04 23:21:16 -0700

    feat: increase lighthouse performance score
    Removed local restaurants.json database.
    Using webp instead of jpg.
    Add media attribute to responsive_index.css.

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -210,7 +210,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`./img/${restaurant.photograph}.jpg`);
+    return (`./img/${restaurant.photograph}.webp`);
   }
 
   /**
